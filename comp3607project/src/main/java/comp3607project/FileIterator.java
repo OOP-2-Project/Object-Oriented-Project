10d3c0b62decc2cf7c5cdf91078fb58f7714946b
Mitchell <mitchell.baboolal@my.uwi.edu>
2023-11-21 17:50:21 -0400

    Fixed iterator

--- comp3607project/src/main/java/comp3607project/FileIterator.java
+++ comp3607project/src/main/java/comp3607project/FileIterator.java
@@ -4,13 +4,13 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileIterator implements InterfaceIterator{
-        private static int index;
-    private List <File> files;
+public class FileIterator implements InterfaceIterator {
+    private int index;
+    private List<File> files;
     private String[] studentFiles;
     private File folder;
 
-    public FileIterator(String folderPath){
+    public FileIterator(String folderPath) {
         this.folder = new File(folderPath);
         this.studentFiles = this.folder.list();
         this.index = 0;
@@ -18,39 +18,26 @@ public class FileIterator implements InterfaceIterator{
         this.next();
     }
 
-    public boolean hasNext(){
+    public boolean hasNext() {
         return index < studentFiles.length;
     }
 
-    public Object next() {
-        if (this.hasNext()) {
+    public List<File> next() {
+        while (this.hasNext()) {
             File nextFile = new File(folder, studentFiles[index]);
-    
-            while (!isTestFile(nextFile)) {
-                index++;
-    
-                if (this.hasNext()) {
-                    nextFile = new File(folder, studentFiles[index]);
-                } else {
-                    break;
-                }
-            }
-    
-            if (isTestFile(nextFile)) {
+
+            if (endsWith(".class", nextFile)) {
                 files.add(nextFile);
-                System.out.println("Files: " + files);
-            } else {
-                System.out.println("Files Collection empty");
+                System.out.println("\n" + "Added Files: " + nextFile.getName());
             }
+
+            index++;
         }
-    
+
         return files;
     }
-    
-    private boolean isTestFile(File file) {
-        return file.getName().equals("PassengerTest.java") ||
-                file.getName().equals("LuggageSlipTest.java") ||
-                file.getName().equals("LuggageManifestTest.java") ||
-                file.getName().equals("FlightTest.java");
+
+    private boolean endsWith(String suffix, File file) {
+        return file.getName().endsWith(suffix);
     }
 }
